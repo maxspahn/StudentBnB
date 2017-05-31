9ab9dbfa5d02ff492bdcc92a909b5db41f00b227
arturogc <artu_95@hotmail.com>
2017-05-31 15:21:01 +0200

    profile

--- app/src/main/java/com/example/maxspahn/studentbnb/ProfileActivity.java
+++ app/src/main/java/com/example/maxspahn/studentbnb/ProfileActivity.java
@@ -8,13 +8,24 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private ListView listView;
+    private TextView nameText;
+    private TextView residenceCity;
+    private TextView residenceName;
+    private TextView myResidence;
+    private ImageView profileImage;
+    private ImageView roomImage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +35,7 @@ public class ProfileActivity extends AppCompatActivity {
 
         listView = (ListView) findViewById(R.id.listView);
 
-        String[] profileElements = {"Evaluations", "My Trips", "My info"};
+        String[] profileElements = {"Evaluations", "My Trips"};
 
         ListAdapter profileAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, profileElements);
         listView.setAdapter(profileAdapter);
@@ -35,6 +46,21 @@ public class ProfileActivity extends AppCompatActivity {
                 Toast.makeText(ProfileActivity.this, option, Toast.LENGTH_SHORT).show();
             }
         });
+
+        // Set the information of the views
+        // We first find the views by id
+        nameText = (TextView) findViewById(R.id.nameText);
+        residenceCity = (TextView) findViewById(R.id.residenceCity);
+        residenceName = (TextView) findViewById(R.id.residenceName);
+        myResidence = (TextView) findViewById(R.id.myResidence);
+        profileImage = (ImageView) findViewById(R.id.profileImage);
+        roomImage = (ImageView) findViewById(R.id.roomImage);
+
+
+        myResidence.setText("My Room");
+        
+
+
 
     }
 
